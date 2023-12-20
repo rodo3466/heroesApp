3d2220f32a9677c8b0d4dd2f00a055d56bfd67fb
Rodolfo <rodolfo.silvaa@idiem.cl>
2023-12-20 10:13:06 -0300

    20-12

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout-page.component.css']
 })
 export class LayoutPageComponent {
-
+  public sidebarItems = [
+    {
+      label: 'Listado',
+      icon: 'label',
+      url: './list'
+    },
+    {
+      label: 'Añadir',
+      icon: 'add',
+      url: './new-hero'
+    },
+    {
+      label: 'Buscar',
+      icon: 'search',
+      url: './search'
+    },
+  ]
 }
